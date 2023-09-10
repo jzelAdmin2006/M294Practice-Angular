4776a4a1fcb0d48132bed5093cb0cf1039acd702
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-09-10 21:33:48 +0200

    recreate hello world

--- HelloWorld/src/app/app.component.spec.ts
+++ HelloWorld/src/app/app.component.spec.ts
@@ -1,10 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: any;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [FormsModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +25,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'HelloWorld'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('HelloWorld');
-  });
+  it('should display message in h1 and input field', () => {
+    expect(compiled.querySelector('h1').textContent).toContain('Hello World!');
+    expect(compiled.querySelector('input').value).toBe('Hello World!');
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const input = compiled.querySelector('input');
+    input.value = 'Neue Nachricht';
+    input.dispatchEvent(new Event('input'));
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('HelloWorld app is running!');
+
+    expect(component.msg).toBe('Neue Nachricht');
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Neue Nachricht'
+    );
   });
 });
