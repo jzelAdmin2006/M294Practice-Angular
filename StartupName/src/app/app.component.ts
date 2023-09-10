b278355d7eea84f8d3c67d74e791ba917f03b134
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-09-10 21:04:57 +0200

    solve task StartupName

--- StartupName/src/app/app.component.ts
+++ StartupName/src/app/app.component.ts
@@ -3,8 +3,55 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'StartupName';
+  name: string = '???';
+
+  prefixes = [
+    'Hype',
+    'Biz',
+    'Lolz',
+    'Rofl',
+    'Galaxy',
+    'Biz',
+    'Fan',
+    'Buzz',
+    'Real',
+    'Solid',
+    'Inter',
+    'Active',
+    'Swoosh',
+  ];
+
+  suffixes = [
+    'park',
+    'span',
+    'cloud',
+    'loop',
+    'verse',
+    'layer',
+    'er',
+    'r',
+    'dot',
+    'dock',
+    'space',
+    'yard',
+    'scale',
+    'signal',
+    'press',
+    'ware',
+    'port',
+  ];
+
+  generate() {
+    this.name = this.getRandomName();
+  }
+
+  getRandomName() {
+    return (
+      this.prefixes[Math.floor(Math.random() * this.prefixes.length)] +
+      this.suffixes[Math.floor(Math.random() * this.suffixes.length)]
+    );
+  }
 }
