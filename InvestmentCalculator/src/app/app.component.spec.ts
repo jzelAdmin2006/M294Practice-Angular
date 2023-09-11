70365341638b33c66cc0a65fa564dab1ec1c751a
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-09-11 19:52:25 +0200

    solve task WeatherStation

--- InvestmentCalculator/src/app/app.component.spec.ts
+++ InvestmentCalculator/src/app/app.component.spec.ts
@@ -1,27 +1,71 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'InvestmentCalculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('InvestmentCalculator');
+  it('should have initial bitcoin price of 16914', (done) => {
+    component.bitcoinPrice$.subscribe((value) => {
+      expect(value).toBe(16914);
+      done();
+    });
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('InvestmentCalculator app is running!');
+  it('should calculate the correct balance', (done) => {
+    component.balance$.subscribe((balance) => {
+      expect(balance).toBe(15);
+      done();
+    });
+  });
+
+  it('should calculate the correct invested amount', (done) => {
+    component.invested$.subscribe((invested) => {
+      expect(invested).toBe(230000);
+      done();
+    });
+  });
+
+  it('should calculate the current value of investments', (done) => {
+    component.valueToday$.subscribe((valueToday) => {
+      expect(valueToday).toBe(253710);
+      done();
+    });
+  });
+
+  it('should calculate the correct win/loss', (done) => {
+    component.win$.subscribe((win) => {
+      expect(win).toBe(23710);
+      done();
+    });
+  });
+
+  it('should add a new investment when "add" method is called', () => {
+    component.inputCount = 5;
+    component.inputPrice = 30000;
+    component.inputDate = new Date(2023, 1, 1);
+
+    component.add();
+
+    component.investments$.subscribe((investments) => {
+      const lastInvestment = investments[investments.length - 1];
+      expect(investments.length).toBe(4);
+      expect(lastInvestment.count).toBe(5);
+      expect(lastInvestment.price).toBe(30000);
+      expect(lastInvestment.date).toEqual(new Date(2023, 1, 1));
+    });
   });
 });
--- InvestmentCalculator/src/app/app.component.ts
+++ InvestmentCalculator/src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'InvestmentCalculator';
+  private bitcoinPriceSubject = new BehaviorSubject<number>(16914);
+  bitcoinPrice$ = this.bitcoinPriceSubject.asObservable();
+
+  private investmentsSubject = new BehaviorSubject<any[]>([
+    { count: 4, price: 20000, date: new Date(2020, 1, 1) },
+    { count: 4, price: 20000, date: new Date(2021, 1, 1) },
+    { count: 7, price: 10000, date: new Date(2022, 1, 1) },
+  ]);
+  investments$ = this.investmentsSubject.asObservable();
+
+  balance$: Observable<number> = this.investments$.pipe(
+    map((investments) =>
+      investments.reduce((acc, investment) => acc + investment.count, 0)
+    )
+  );
+
+  invested$: Observable<number> = this.investments$.pipe(
+    map((investments) =>
+      investments.reduce(
+        (acc, investment) => acc + investment.count * investment.price,
+        0
+      )
+    )
+  );
+
+  valueToday$: Observable<number> = combineLatest([
+    this.balance$,
+    this.bitcoinPrice$,
+  ]).pipe(map(([balance, price]) => balance * price));
+
+  win$: Observable<number> = combineLatest([
+    this.valueToday$,
+    this.invested$,
+  ]).pipe(map(([valueToday, invested]) => valueToday - invested));
+
+  inputCount: number = 0;
+  inputPrice: number = 0;
+  inputDate: Date = new Date();
+
+  add() {
+    alert(
+      `Hinzugefügt: ${this.inputCount} BTC
+      zum Preis von ${this.inputPrice} CHF`
+    );
+    this.investmentsSubject.next([
+      ...this.investmentsSubject.getValue(),
+      {
+        count: Number(this.inputCount),
+        price: Number(this.inputPrice),
+        date: new Date(this.inputDate),
+      },
+    ]);
+  }
 }
