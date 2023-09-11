aa7e6ce9127deb8983d38ecd4f7c78d8b16c0253
johannes.zeller <johannes.zeller@bison-group.com>
2023-09-11 16:14:46 +0200

    solve WeatherStation Task

--- WeatherStation/src/app/app.component.spec.ts
+++ WeatherStation/src/app/app.component.spec.ts
@@ -1,10 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +23,37 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WeatherStation'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('WeatherStation');
+  it('should increase temperature by 1 when warmer is called', () => {
+    component.warmer();
+
+    expect(component.temperatureCelsiusSubject.value).toEqual(1);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('WeatherStation app is running!');
+  it('should decrease temperature by 1 when colder is called', () => {
+    component.colder();
+
+    expect(component.temperatureCelsiusSubject.value).toEqual(-1);
+  });
+
+  it('isFreezing$ should emit true when temperature is below 0', () => {
+    component.colder();
+
+    component.isFreezing$.subscribe((isFreezing) => {
+      expect(isFreezing).toBeTrue();
+    });
+  });
+
+  it('isFreezing$ should emit false when temperature is 0 or above', () => {
+    function expectNotFreezing() {
+      component.isFreezing$.subscribe((isFreezing) => {
+        expect(isFreezing).toBeFalse();
+      });
+    }
+
+    expectNotFreezing();
+
+    component.warmer();
+
+    expectNotFreezing();
   });
 });
--- WeatherStation/src/app/app.component.ts
+++ WeatherStation/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'WeatherStation';
+  temperatureCelsiusSubject = new BehaviorSubject<number>(0);
+
+  temperatureFahrenheit$ = this.temperatureCelsiusSubject.pipe(
+    map((temp) => this.roundTo2Digits(temp * 1.8 + 32))
+  );
+
+  temperatureKelvin$ = this.temperatureCelsiusSubject.pipe(
+    map((temp) => this.roundTo2Digits(temp + 273.15))
+  );
+
+  isFreezing$ = this.temperatureCelsiusSubject.pipe(map((temp) => temp < 0));
+
+  colder(): void {
+    this.temperatureCelsiusSubject.next(
+      this.temperatureCelsiusSubject.value - 1
+    );
+  }
+
+  warmer(): void {
+    this.temperatureCelsiusSubject.next(
+      this.temperatureCelsiusSubject.value + 1
+    );
+  }
+
+  private roundTo2Digits(value: number): number {
+    return Math.round(value * 100) / 100;
+  }
 }
